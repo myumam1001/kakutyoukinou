23ae45864fa420691819fec72cfd55faaf722062
myumam1001 <myumam1001@gmail.com>
2025-02-03 16:48:38 +0900

    更新 main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,12 +1,9 @@
+OLED12864_I2C.init(60)
 //% color=190 weight=100 icon="\uf06b" block="自作拡張機能OLED表示超音波センサー"
 namespace 自作拡張機能OLED表示超音波センサー {
     //% block
-    export function 超音波センサーの端子を設定する (t: number) {
-        
-    }
-    //% block
-    export function camlCaseTwo() {
-
+    export function 超音波センサーの値をoledに表示するtはｐ１eはｐ２ () {
+        OLED12864_I2C.showNumber(0, 0, sonar.ping(DigitalPin.P1, DigitalPin.P2, PingUnit.MicroSeconds), 1)
     }
     
 }
